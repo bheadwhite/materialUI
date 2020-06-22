5120d0d0dc85b68b661e31c5d14f720951a61400
Brent Whitehead <brent.whitehead@tcnbroadcasting.com>
2020-06-22 14:13:35 -0600

    added project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import { Error, FileCopy } from "@material-ui/icons"
+import { TextField, Button, Switch, makeStyles } from "@material-ui/core"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+//how to import react-toastify
+//material-ui icons/ prebuilt components
+//material-ui styles using makeStyles
+
+const useStyles = makeStyles(() => ({
+  btn: {
+    background: "green",
+    color: "white",
+    marginLeft: "1rem",
+  },
+}))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [field, setField] = useState("")
+  const [checked, setChecked] = useState(false)
+  const classes = useStyles()
+
+  const handleChecked = () => {
+    setChecked(!checked)
+    toast.error(<ErrorMessage>you have set the taost to be {checked.toString()}</ErrorMessage>)
+    toast.info("this is blue")
+    toast.warn("this is yellow")
+  }
+
+  const ErrorMessage = ({ children }) => (
+    <div>
+      <Error />
+      {children}
     </div>
-  );
+  )
+
+  return (
+    <div className='App'>
+      <form>
+        <h1>This is my Form</h1>
+        <FileCopy />
+        <Switch
+          checked={checked}
+          onChange={handleChecked}
+          color='primary'
+          name='checkedB'
+          inputProps={{ "aria-label": "primary checkbox" }}
+        />
+        <TextField value={field} onChange={(e) => setField(e.target.value)} />
+        <input value={field} onChange={(e) => setField(e.target.value)} />
+        <Button type='submit' className={classes.btn}>
+          Submit
+        </Button>
+      </form>
+      <ToastContainer />
+    </div>
+  )
 }
 
-export default App;
+export default App
